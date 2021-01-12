8d2be0964b9a6ced0d8c75d2061762511310aa73
TAKAE1117 <takayuki.ogawa.0712@gmail.com>
2021-01-12 21:03:02 +0900

    Issue6_fourth time

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -36,22 +36,14 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
-      wday_num = Date.today.wday
+      wday_num = Date.today.wday + x
       if wday_num >= 7
-        wday_num = wday_num -7
+        wday_num = wday_num - 7
       end
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => (@todays_date + x).wday}
+      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[wday_num]}
       @week_days.push(days)
     end
 
   end
 end
-
-# Issue1
-#days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
-#一応、元のコードを書いております。
-# ""を外しました。
-
-# Issue2
-# getWeek → get_weekに変更
